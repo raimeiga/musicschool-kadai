83759c3d7f5f1dd4b5a5bf742f0ca8e6a5a370e5
raimeiga <yusuke612@hotmail.com>
2024-11-04 01:36:09 +0900

    7-8-3：single-blog.phpにテンプレートタグを追加しよう」でブログ記事から必要事項を取得するテンプレを記述。正しく反映された。

--- single-blog.php
+++ single-blog.php
@@ -81,32 +81,61 @@
                
           
           <div class="p-blog-details-main__articles">
-            <a href="blog_details.html">
+            <?php
+              $prev_post = get_previous_post();
+              $next_post = get_next_post();
+            ?>
+            <?php if ( !empty($prev_post) ): ?>
+            <a href="<?php echo get_permalink($prev_post->ID); ?>">
               <div class="p-blog-details-main__article">
                 <div class="p-blog-details-main__article-item-01">
                   <p class="p-blog-details-main__pre-article">◀︎ 前の記事</p>
                 </div>
                 <div class="p-blog-details-main__article-item-02">
+                  <?php if(has_post_thumbnail()): ?>
                   <div class="p-blog-details-main__article-image pc">
-                    <img src="<?php echo get_template_directory_uri(); ?>/images/blog-details98.jpg" alt="フォロワーではなくファンを増やせとは？">
+                    <img src="<?php echo get_the_post_thumbnail_url($prev_post->ID); ?>" alt="フォロワーではなくファンを増やせとは？">
                   </div>
-                  <p class="p-blog-details-main__article-title u-flex-1">タイトルが入ります。タイトルが入ります。タイトルが入</p>
+                  <?php endif; ?>
+                  <p class="p-blog-details-main__article-title u-flex-1">
+                  <?php if(mb_strlen($prev_post->post_title)>32) {
+                    $title= mb_substr($prev_post->post_title,0,32); echo $title . '...';
+                    } else {
+                    echo $prev_post->post_title;
+                    } ?>
+                  </p>
                 </div>
               </div>
             </a>
-            <a href="blog_details.html">
+            <?php else: ?>
+            <div class="p-blog-details-main__article"></div>
+            <?php endif; ?>
+            
+            <?php if ( !empty($next_post) ): ?>
+            <a href="<?php echo get_permalink($next_post->ID); ?>">
              <div class="p-blog-details-main__article">
               <div class="p-blog-details-main__article-item-01">
                 <p class="p-blog-details-main__next-article">次の記事 ▶︎</p>
               </div>
               <div class="p-blog-details-main__article-item-02">
+                <?php if(has_post_thumbnail()): ?>
                 <div class="p-blog-details-main__article-image pc">
                   <img src="<?php echo get_template_directory_uri(); ?>/images/blog-details98.jpg" alt="フォロワーではなくファンを増やせとは？">
                 </div>
-                <p class="p-blog-details-main__article-title u-flex-1">タイトルが入ります。タイトルが入ります。タイトルが入</p>
+                <?php endif; ?>
+                <p class="p-blog-details-main__article-title u-flex-1">
+                <?php if(mb_strlen($next_post->post_title)>32) {
+                $title= mb_substr($next_post->post_title,0,32); echo $title . '...';
+                } else {
+                echo $next_post->post_title;
+                } ?>
+                </p>
               </div>
             </div>
             </a>
+            <?php else: ?>
+             <div class="p-blog-details-main__article"></div>
+            <?php endif; ?>
           </div>    
 
           <div class="p-blog-details-main__related-articles p-blog-details-main-related-articles">
