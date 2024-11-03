3b6b456d4968a49e9b9507e04ee0a6787cf8a028
raimeiga <yusuke612@hotmail.com>
2024-11-04 08:08:19 +0900

    7-9-3：取得したデータを表示しよう」まで整った状態

--- single-blog.php
+++ single-blog.php
@@ -82,56 +82,57 @@
           <!-- 前後記事のテンプレパーツ読み込み -->
           <?php get_template_part('template-parts/single-arrows-area', '', $args = array('image_area' => 'has', 'post_type' => 'blog')); ?>
 
-          
           <div class="p-blog-details-main__related-articles p-blog-details-main-related-articles">
+            <?php
+            $terms = get_the_terms($post->ID, 'blog_tag');
+            if(!empty($terms)):
+            foreach ( $terms as $term ):
+              $args = array(
+                'posts_per_page' => 3,
+                'post_type' => 'blog',
+                'taxonomy' => 'blog_tag',
+                'term' => $term->slug,
+                'orderby' => 'date',
+                'order' => 'DESC'
+              );
+              $the_query = new WP_Query($args);
+            ?>
             <div class="p-blog-details-main-related-articles__label">
               <p>関連記事</p>
-            </div>            
-            <div class="p-blog-details-main-related-article__lists">              
-                <div class="p-blog-details-main-related-article__list">
+            </div>                       
+            <div class="p-blog-details-main-related-article__lists">   
+              <?php if( $the_query->have_posts() ):
+              while( $the_query->have_posts() ): $the_query->the_post(); ?> 
+                <a href="<?php echo get_permalink($post->ID); ?>" class="p-blog-details-main-related-article__list">
                   <div class="p-blog-details-main-related-article__image">
+                    <?php
+                    $thumbnail_id = get_post_thumbnail_id($post->ID);
+                    $thumb_url = wp_get_attachment_image_src($thumbnail_id, 'small');
+                    if ( get_post_thumbnail_id($post->ID) ):
+                    ?>
                     <picture>
                       <source media="(max-width: 767px)" srcset="<?php echo get_template_directory_uri(); ?>/images/blog-details-sp160.110.jpg">
                       <img src="<?php echo get_template_directory_uri(); ?>/images/blog-details200.140.jpg" alt="フォロワーではなくファンを増やせとは？">
                     </picture>
+                    <?php endif; ?>
                   </div>
-                  <p class="c-caption c-caption--w80">ギター</p>
+                  <p class="c-caption c-caption--w80"><?php echo $term->name; ?></p>
                   <div class="p-blog-details-main-related-article__text u-flex-1">
-                    <p>タイトルが入ります。タイトル</p>
-                    <time datetime="2024-10-01" class="p-blog-details-main__date">2024.10.01</time>
+                    <p>
+                    <?php if(mb_strlen($post->post_title)>32) 
+                    {$title= mb_substr($post->post_title,0,32); echo $title . '...';} 
+                    else {echo $post->post_title;} ?>
+                    </p>
+                    <time datetime="the_time('Y-m-d')" class="p-blog-details-main__date"><?php the_time('Y.m.d') ?></time>
                   </div>                     
-                </div>
-              
-                <div class="p-blog-details-main-related-article__list">
-                  <div class="p-blog-details-main-related-article__image">
-                    <picture>
-                      <source media="(max-width: 767px)" srcset="<?php echo get_template_directory_uri(); ?>/images/blog-details-sp160.110.jpg">
-                      <img src="<?php echo get_template_directory_uri(); ?>/images/blog-details200.140.jpg" alt="フォロワーではなくファンを増やせとは？">
-                    </picture>
-                  </div>
-                  <p class="c-caption c-caption--w80">ギター</p>
-                  <div class="p-blog-details-main-related-article__text u-flex-1">
-                    <p>タイトルが入ります。タイトル</p>
-                    <time datetime="2024-10-01" class="p-blog-details-main__date">2024.10.01</time>
-                  </div>                     
-                </div>
-              
-                <div class="p-blog-details-main-related-article__list">
-                 <div class="p-blog-details-main-related-article__image">
-                   <picture>
-                     <source media="(max-width: 767px)" srcset="<?php echo get_template_directory_uri(); ?>/images/blog-details-sp160.110.jpg">
-                     <img src="<?php echo get_template_directory_uri(); ?>/images/blog-details200.140.jpg" alt="フォロワーではなくファンを増やせとは？">
-                   </picture>
-                 </div>
-                 <p class="c-caption c-caption--w80">ギター</p>
-                 <div class="p-blog-details-main-related-article__text u-flex-1">
-                   <p>タイトルが入ります。タイトル</p>
-                   <time datetime="2024-10-01" class="p-blog-details-main__date">2024.10.01</time>
-                 </div>                     
-                </div>
-              
+                </a>
+              <?php endwhile; ?>
+              <?php wp_reset_postdata(); ?>
+              <?php endif; ?>
+              <?php endforeach; ?>
+              <?php endif; ?>  
             </div>
-          </div>      
+          </div>          
         </div><!-- p-blog-details__mainの終わり -->
 
         <!-- サイドバーのテンプレ読み込み -->
