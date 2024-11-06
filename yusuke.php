74376617360c1f20598e725bb2cbc0f6069f3011
raimeiga <yusuke612@hotmail.com>
2024-11-06 16:34:32 +0900

    10-5：ページャーの表示を確認しよう」まで終わった状態

--- yusuke.php
+++ yusuke.php
@@ -1,59 +1,102 @@
-<div class="c-col-main--arrow">
+
+<section class="result-list">
   <?php
-  $prev_post = get_previous_post();
-  $next_post = get_next_post();
+  $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+  $args = array(
+      'paged' => $paged,
+      'post_type' => 'result',
+      'orderby' => 'date',
+      'order' => 'DESC'
+  );
+  $the_query = new WP_Query($args);
   ?>
-  <?php if ( !empty($prev_post) ): ?>
-  <a href="<?php echo get_permalink($prev_post->ID); ?>" 
-   class="c-col-main--arrow-prev c-col-main--arrow-blog">
-    <p class="c-blog-section-name c-blog-section-name--arrow-prev">◀︎ 前の記事</p>
-    <div class="c-arrow-block">
-      <?php if(has_post_thumbnail()): ?>
-      <div class="c-arrow-block__img-area">
-        <p class="c-arrow-block__img">
-          <img src="<?php echo get_the_post_thumbnail_url($prev_post->ID); ?>" alt="">
-        </p>
-      </div>
-      <?php endif; ?>
-      <div class="c-arrow-block__text-area">
-        <p class="c-arrow-block__title">
-          <?php if(mb_strlen($prev_post->post_title)>32) {
-          $title= mb_substr($prev_post->post_title,0,32); echo $title . '...';
-          } else {
-          echo $prev_post->post_title;
-          } ?>
-        </p>
-      </div>
+  <div class="content-area">
+    <h2 class="c-head-lower">卒業実績一覧</h2>
+    <?php if($the_query->have_posts()):
+    $i = 0;
+    $total_count = $the_query->post_count;
+    while($the_query->have_posts()): $the_query->the_post();
+    if($i % 2 == 0):
+    ?>
+    <div class="result-list__row">
+      <a href="<?php echo get_permalink($post->ID); ?>" 
+      class="c-result-block c-result-block--list">
+        <div class="c-result-block__img-area c-result-block__img-area--list">
+          <p class="c-result-block__img c-result-block__img--list">
+            <?php
+            $thumbnail_id = get_post_thumbnail_id($post->ID);
+            $thumb_url = wp_get_attachment_image_src($thumbnail_id, 'small');
+            if (get_post_thumbnail_id($post->ID)):
+            ?>
+            <img src="<?php echo $thumb_url[0] ?>" alt="">
+            <?php endif; ?>
+          </p>
+          <p class="c-result-block__tag">
+          <?php
+          $terms = get_the_terms($post->ID, 'result_tag');
+          foreach ($terms as $term):
+              echo $term->name;
+          endforeach;
+          ?>
+          </p>
+        </div>
+        <div class="c-result-block__text-area">
+          <p class="c-result-block__title c-result-block__title--list">
+            <?php if(mb_strlen($post->post_title)>33) 
+            {$title= mb_substr($post->post_title,0,33); echo $title . '...';
+            } else {echo $post->post_title;} ?>
+          </p>
+          <p class="c-result-block__date c-result-block__date--list">
+            <time datetime="the_time('Y-m-d')">
+              <?php the_time('Y.m.d') ?>
+            </time>
+          </p>
+        </div>
+      </a>
+      <?php if($i == $total_count - 1): ?>
     </div>
-  </a>
-  <?php else: ?>
-  <p class="c-col-main--arrow-prev c-col-main--arrow-blog"></p>
-  <?php endif; ?>
-  
-  <?php if ( !empty($next_post) ): ?>
-  <a href="<?php echo get_permalink($next_post->ID); ?>" 
-  class="c-col-main--arrow-next c-col-main--arrow-blog">
-    <p class="c-blog-section-name c-blog-section-name--arrow-next">次の記事 ▶︎</p>
-    <div class="c-arrow-block">
-      <?php if(has_post_thumbnail()): ?>
-      <div class="c-arrow-block__img-area">
-        <p class="c-arrow-block__img">
-          <img src="<?php echo get_the_post_thumbnail_url($next_post->ID); ?>" alt="">
-        </p>
-      </div>
       <?php endif; ?>
-      <div class="c-arrow-block__text-area">
-        <p class="c-arrow-block__title">
-          <?php if(mb_strlen($next_post->post_title)>32) {
-          $title= mb_substr($next_post->post_title,0,32); echo $title . '...';
-          } else {
-          echo $next_post->post_title;
-          } ?>
-        </p>
-      </div>
+    <?php else: ?>
+      <a href="<?php echo get_permalink($post->ID); ?>" 
+      class="c-result-block c-result-block--list">
+        <div class="c-result-block__img-area c-result-block__img-area--list">
+          <p class="c-result-block__img c-result-block__img--list">
+            <?php
+            $thumbnail_id = get_post_thumbnail_id($post->ID);
+            $thumb_url = wp_get_attachment_image_src($thumbnail_id, 'small');
+            if (get_post_thumbnail_id($post->ID)):
+            ?>
+            <img src="<?php echo $thumb_url[0] ?>" alt="">
+            <?php endif; ?>
+          </p>
+          <p class="c-result-block__tag">
+            <?php
+            $terms = get_the_terms($post->ID, 'result_tag');
+            foreach ($terms as $term):
+                echo $term->name;
+            endforeach;
+            ?>
+          </p>
+        </div>
+        <div class="c-result-block__text-area">
+          <p class="c-result-block__title c-result-block__title--list">
+            <?php if(mb_strlen($post->post_title)>33) 
+            {$title= mb_substr($post->post_title,0,33); echo $title . '...';
+            } else {echo $post->post_title;} ?>
+          </p>
+          <p class="c-result-block__date c-result-block__date--list">
+            <time datetime="the_time('Y-m-d')">
+              <?php the_time('Y.m.d') ?>
+            </time>
+          </p>
+        </div>
+      </a>
     </div>
-  </a>
-  <?php else: ?>
-  <p class="c-col-main--arrow-next c-col-main--arrow-blog"></p>
-  <?php endif; ?>
-</div>
+    <?php endif; ?>
+    <?php $i++; ?>
+    <?php endwhile; ?>
+    <?php wp_reset_postdata(); ?>
+    <?php endif; ?>
+  </div>
+  <?php get_template_part('template-parts/pager', '', $the_query); ?>
+</section>
