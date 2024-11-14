2221df89f0c28655f9e4c58dd758569b86e8aa31
raimeiga <yusuke612@hotmail.com>
2024-11-14 12:20:26 +0900

    WordPressの既存のjQueryのみを読み込ませるように2行をコメントアウト。エラーが消えた。

--- functions.php
+++ functions.php
@@ -20,8 +20,8 @@ add_action('wp_enqueue_scripts', 'custom_theme_scripts');
 
 function custom_theme_footer_scripts() {
     // jQueryを読み込む
-    wp_deregister_script('jquery'); // 既存のjQueryを解除
-    wp_enqueue_script('jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js', array(), null, true); // jQueryを追加
+    // wp_deregister_script('jquery'); // 既存のjQueryを解除
+    // wp_enqueue_script('jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js', array(), null, true); // jQueryを追加
 
     // slickのJavaScript
     wp_enqueue_script('slick-js', 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js', array('jquery'), null, true);
