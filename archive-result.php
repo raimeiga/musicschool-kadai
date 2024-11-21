7b298009bff1da359a01b602e984b3828f220eec
raimeiga <yusuke612@hotmail.com>
2024-11-22 02:56:29 +0900

    調整

--- archive-result.php
+++ archive-result.php
@@ -30,64 +30,47 @@
 
         <h2 class="c-section-title25" >卒業実績一覧</h2>
         <div class="p-result-list__lists">
-         <!-- 記事の取得 -->
-         <?php if (have_posts()) : 
-        $counter = 0;  // 記事のインデックスをカウント
-        while (have_posts()) : the_post(); 
-        $counter++; // 投稿のインデックスをカウント
-      ?>
-        <a href="<?php the_permalink(); ?>" class="p-result-list__list p-result-list-list">
-          <div class="p-result-list-list__image">
-            <?php 
-              if (has_post_thumbnail()) {
-                // アイキャッチ画像が設定されていればその画像を表示
-                the_post_thumbnail('custom-size'); // 'custom-size'はカスタムサイズ
-              } else {
-                // アイキャッチ画像が設定されていない場合、ダミー画像を表示
-                echo '<img src="' . get_template_directory_uri() . '/images/dummy-image.jpg" alt="ダミー画像">';
-              }
+          <!-- 記事の取得 -->
+          <?php if (have_posts()) : 
+            $counter = 0;  // 記事のインデックスをカウント
+            while (have_posts()) : the_post(); 
+            $counter++; // 投稿のインデックスをカウント
             ?>
-          </div>
-          <p class="c-caption c-caption--w100">
-            <?php if( has_term('', 'result_tag', $post->ID) ): 
-              $post_term = get_the_terms($post->ID, 'result_tag'); 
-              echo $post_term[0]->name; 
-            endif; ?>
-          </p>
-          <!-- 最初の2つの記事には特別なクラスを付け、それ以降の記事には別のクラスを付ける -->
-<?php if ($counter <= 2) : ?>
-  <h3 class="p-result-list-list__title--pad20">
-    <?php 
-      $title = get_the_title(); // タイトルを取得
-      if (wp_is_mobile()) {
-        // スマートフォンの場合は48文字まで表示
-        echo mb_substr($title, 0, 48) . '...';
-      } else {
-        // PCの場合は66文字まで表示
-        echo mb_substr($title, 0, 66) . '...';
-      }
-    ?>
-  </h3>
-<?php else : ?>
-  <h3 class="p-result-list-list__title">
-    <?php 
-      $title = get_the_title(); // タイトルを取得
-      if (wp_is_mobile()) {
-        // スマートフォンの場合は48文字まで表示
-        echo mb_substr($title, 0, 48) . '...';
-      } else {
-        // PCの場合は66文字まで表示
-        echo mb_substr($title, 0, 66) . '...';
-      }
-    ?>
-  </h3>
-<?php endif; ?>
-
-
-          <time datetime="the_time('Y-m-d')" class="p-result-list-list__time"><?php the_time('Y.m.d'); ?></time>
-        </a>
-      <?php endwhile; endif; ?>           
-    </div>
+           <a href="<?php the_permalink(); ?>" class="p-result-list__list p-result-list-list">
+             <div class="p-result-list-list__image">
+               <?php 
+                 if (has_post_thumbnail()) {
+                   // アイキャッチ画像が設定されていればその画像を表示
+                   the_post_thumbnail('custom-size'); // 'custom-size'はカスタムサイズ
+                 } else {
+                   // アイキャッチ画像が設定されていない場合、ダミー画像を表示
+                   echo '<img src="' . get_template_directory_uri() . '/images/dummy-image.jpg" alt="ダミー画像">';
+                 }
+               ?>
+             </div>
+             <p class="c-caption c-caption--w100">
+               <?php if( has_term('', 'result_tag', $post->ID) ): 
+                 $post_term = get_the_terms($post->ID, 'result_tag'); 
+                 echo $post_term[0]->name; 
+               endif; ?>
+             </p>
+             <!-- 最初の2つの記事には特別なクラスを付け、それ以降の記事には別のクラスを付ける -->
+             <?php 
+               $title = get_the_title(); // タイトルを取得
+               // タイトルが35文字より長い場合、35文字に切り取り、'...'を追加
+               if (mb_strlen($title) > 35) {
+                 $title = mb_substr($title, 0, 35) . '...';
+               }
+             ?>
+             <?php if ($counter <= 2) : ?>
+               <h3 class="p-result-list-list__title--pad20"><?php echo $title; ?></h3>
+             <?php else : ?>
+               <h3 class="p-result-list-list__title"><?php echo $title; ?></h3>
+             <?php endif; ?>
+             <time datetime="the_time('Y-m-d')" class="p-result-list-list__time"><?php the_time('Y.m.d'); ?></time>
+           </a>
+          <?php endwhile; endif; ?>           
+        </div>
         <!-- ページャーのテンプレ読み込み -->
         <?php get_template_part('template-parts/pager', '', $the_query); ?>
       </div><!-- p-result-list__inner l-innerの終わり -->
