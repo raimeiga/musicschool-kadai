fac32f219391b08d46c2ebf9d5909caea2a95000
raimeiga <yusuke612@hotmail.com>
2024-11-24 01:16:16 +0900

    descriptionを動的に修正

--- header.php
+++ header.php
@@ -7,9 +7,9 @@
   <meta name="robots" content="noindex">
   <?php if(is_front_page()): ?>
     <title>きたむらミュージックスクール | 「音楽で生きる」を叶える ミュージックスクール</title>
-    <meta name="description" content="「音楽で生きる」を叶える ミュージックスクール
-    「きたむらミュージックスクール」の公式ホームページです。">
-  <!-- 固定ページ -->
+    <meta name="description" content="「音楽で生きる」を叶える ミュージックスクール「きたむらミュージックスクール」の公式ホームページです。">
+  
+    <!-- 固定ページ -->
   <?php elseif(is_page()): ?>
     <?php if(get_field("title")): ?>
     <title><?php echo the_field('title'); ?></title>
@@ -19,11 +19,18 @@
     <?php if(get_field("description")): ?>
     <meta name="description" content="<?php echo the_field('description'); ?>">
     <?php else: ?>
-    <meta name="description" content="きたむらミュージックスクール公式ホームページの
-    <?php the_title(); ?>ページです。">
+    <meta name="description" content="きたむらミュージックスクール公式ホームページの<?php the_title(); ?>ページです。">
     <?php endif; ?>
+    
   <!-- 投稿ページ -->
-  <?php elseif(is_single()): ?>
+<?php elseif(is_single()): ?>
+  <?php if (is_singular('blog')): // single-blog.php の場合 ?>
+    <title>ブログ タイトル「<?php the_title(); ?>」のページです。 | きたむらミュージックスクール</title>
+    <meta name="description" content="きたむらミュージックスクール公式ホームページのブログ「<?php the_title(); ?>」のページです。">
+  <?php elseif (is_singular('result')): // single-result.php の場合 ?>
+    <title>卒業実績 タイトル「<?php the_title(); ?>」のページです。 | きたむらミュージックスクール</title>
+    <meta name="description" content="きたむらミュージックスクール公式ホームページの卒業実績「<?php the_title(); ?>」のページです。">
+  <?php else: ?>
     <?php if(get_field("title")): ?>
     <title><?php echo the_field('title'); ?></title>
     <?php else: ?>
@@ -42,43 +49,64 @@
     ?>
     <?php endwhile; ?><?php endif; ?>">
     <?php endif; ?>
-  <!-- アーカイブページ -->
+  <?php endif; ?>
+
+
+  <!-- アーカイブページ archive-blog.php,archive-result.php,taxonomy-blog_tag.php,taxonomy-result_tag.php-->
   <?php elseif(is_archive()): ?>
-    <?php if(!is_paged()): ?>
-       <?php
-       // タクソノミーが blog_tag か result_tag かを確認する
-       if (is_tax('blog_tag')) {
-           $term = get_queried_object();
-           $title = $term->name; // タクソノミーの名前を取得
-       } elseif (is_tax('result_tag')) {
-           $term = get_queried_object();
-           $title = $term->name; // タクソノミーの名前を取得
-       } else {
-           $title = single_term_title('', false); // デフォルトのタイトル取得
-       }
-       ?>
-       <title><?php echo esc_html($title); ?> | きたむらミュージックスクール</title>
-       <meta name="description" content="きたむらミュージックスクール公式ホームページの
-       <?php echo esc_html($title); ?>タクソノミーアーカイブページです。">
+  <?php if(!is_paged()): ?>
+    <?php
+    // タクソノミーが blog_tag か result_tag かを確認する
+    if (is_post_type_archive('blog')) {
+        // archive-blog.phpの場合
+        $title = 'ブログ一覧';
+    } elseif (is_post_type_archive('result')) {
+        // archive-result.phpの場合
+        $title = '卒業実績一覧';
+    } elseif (is_tax('blog_tag')) {
+        $term = get_queried_object();
+        $title = $term->name; // タクソノミーの名前を取得
+    } elseif (is_tax('result_tag')) {
+        $term = get_queried_object();
+        $title = $term->name; // タクソノミーの名前を取得
+    } else {
+        $title = single_term_title('', false); // デフォルトのタイトル取得
+    }
+    ?>
+    <title><?php echo esc_html($title); ?> | きたむらミュージックスクール</title>
+    <meta name="description" content="きたむらミュージックスクール公式ホームページの<?php echo esc_html($title); ?>のページです。">
     <?php else: ?>
-       <title><?php echo esc_html($title); ?>タクソノミーアーカイブ 
-       <?php show_page_number(''); ?>ページ目  | きたむらミュージックスクール</title>
-       <meta name="description" content="きたむらミュージックスクール公式ホームページの
-       <?php echo esc_html($title); ?>タクソノミーアーカイブ <?php show_page_number(''); ?>ページ目です。">
+      <?php
+      // ページ番号がある場合
+      if (is_post_type_archive('blog')) {
+          // archive-blog.phpの場合
+          $title = 'ブログ一覧';
+      } elseif (is_post_type_archive('result')) {
+          // archive-result.phpの場合
+          $title = '卒業実績一覧';
+      } else {
+          $title = single_term_title('', false); // デフォルトのタイトル取得
+      }
+      ?>
+      <title><?php echo esc_html($title); ?> | きたむらミュージックスクール</title>
+      <meta name="description" content="きたむらミュージックスクール公式ホームページの<?php echo esc_html($title); ?>の<?php show_page_number(''); ?>ページ目です。">
     <?php endif; ?>
+ 
+ 
   <!-- 検索結果ページ -->
   <?php elseif(is_search()): ?>
-    <title>検索結果 | きたむらミュージックスクール</title>
-    <meta name="description" content="きたむらミュージックスクール公式ホームページの検索結果ページです。">
+    <title>検索結果: <?php echo get_search_query(); ?> | きたむらミュージックスクール</title>
+    <meta name="description" content="きたむらミュージックスクール公式ホームページの「<?php echo get_search_query(); ?>」の検索結果ページです。">
+
   <!-- 404ページ -->
   <?php elseif(is_404()): ?>
     <title>お探しのページはございません | きたむらミュージックスクール</title>
     <meta name="description" content="きたむらミュージックスクール公式ホームページの404ページです。">
+
   <!-- その他 -->
   <?php else: ?>
     <title><?php the_title(); ?> | きたむらミュージックスクール</title>
-    <meta name="description" content="きたむらミュージックスクール公式ホームページの
-    <?php the_title(); ?>ページです。">
+    <meta name="description" content="きたむらミュージックスクール公式ホームページの<?php the_title(); ?>ページです。">
   <?php endif; ?>
 
   <!-- Google構造化データ マークアップ -->
@@ -93,7 +121,7 @@
   <?php wp_head(); ?>
 </head>
 
-<body style="display: none;"><!-- ヘッダー遅延・ヘッダーナビメニューの表示崩れ防止　 -->
+<body style="display: none;">
   <div id="wrapper">
     <!-- header -->
     <header class="l-header">
@@ -112,7 +140,6 @@
           </a>   
         </div>
         <div class="p-header__right p-header-right">      
-          <!-- <div class="header__hb-btn header-hb__btn sp"> -->
           <div class="c-hamburger  js-hamburger sp">
             <div class="c-hamburger__inner">
               <span></span>
@@ -135,4 +162,3 @@
       </div>
       <?php wp_head(); ?>
     </header>
-    
